460af0bc087e8d9e726bb9ce39bea76ec3604ab8
vrevt <revtivan@gmail.com>
2020-03-02 16:21:57 +0300

    download true

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ chrome.runtime.onInstalled.addListener(function() {
 	console.log("started");
 
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.fonbet.ru'},
-        })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {hostEquals: 'www.fonbet.ru'},
+			})],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
 	let downloadButton = document.getElementById('download');
 	downloadButton.addEventListener('click', function() {
-		alert('chpunk');
-	});
-
-	//chrome.tabs.executeScript(null, {"code": "document.getElementsByClassName('column column-1')"}, function(data) { })
 		
-	//const data = document.getElementsByClassName('test');
-	//alert(data.innerText);
+		chrome.runtime.onMessage.addListener(
+  			function(request, sender, sendResponse) {
+   			 // LOG THE CONTENTS HERE
+    		let dom = new DOMParser().parseFromString(request.content, "text/html");
+    		console.log(dom);
+
+    		let column = [];
+    		for (let i = 2; i < 6; i++) {
+    			column.push(dom.getElementsByClassName('column column-' + i.toString()));
+			}
+    		let table = [];
+    		for (let i = 0; i < 4; i++){
+    			table.push([]);
+    			for (let j = 0; j < column[i].length; j++) table[i].push(column[i][j].innerText);
+			}
+
+    		table.push([]);
+    		table.push([]);
+    		let col = dom.getElementsByClassName('column column-6');
+    		table[4].push(col[0].innerText);
+    		table[5].push('');
+
+    		for (let i = 1; i < col.length; i++){
+    			let x = col[i].getElementsByClassName('bets-list__sticker-value');
+    			if (x.length > 0){
+    				table[4].push(col[i].getElementsByTagName('span')[0].innerText);
+					table[5].push(x[0].innerText);
+				} else {
+					table[4].push(col[i].getElementsByTagName('span')[0].innerText);
+					table[5].push('');
+				}
+
+			}
+
+
+    		let csvContent = "data:text/csv;charset=utf-8,";
+
+    		for (let i = 0; i < table[0].length; i++){
+    			for (let j = 0; j < 6; j++) {
+    				csvContent += table[j][i] + ',';
+				}
+				csvContent += "\r\n";
+			}
+
+    		let encodedUri = encodeURI(csvContent);
+    		window.open(encodedUri);
+  		});
+
+		chrome.tabs.getSelected(null, function(tab) {
+
+  		// Now inject a script onto the page
+  		chrome.tabs.executeScript(tab.id, {
+      		 code: "chrome.runtime.sendMessage({content: document.body.innerHTML}, function(response) { console.log('success'); });"
+     	}, function() { console.log('done'); });
+	});
+	});
 });
